b0cf52212eaa752d81e9a47da011be9ca4b09e8c
shikubro90 <shikubro90@gmail.com>
2022-06-24 01:09:53 +0600

    changes

--- .history/src/components/footer/footer_20220624010451.jsx
+++ .history/src/components/footer/footer_20220624010451.jsx
@@ -10,7 +10,7 @@ const footer = () => {
             <footer>
                 <a href="#home" className='footer__logo'>Shiku</a>
                 <ul className='permalinks'>
-                    <li><a href="#home">Home</a></li>
+                    <li><a href="#">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#experience">Experience</a></li>
                     <li><a href="#service">Services</a></li>
